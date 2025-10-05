d5732efd61a1f45f4a7c79ff05a63c4f1789b3f4
Garazbolg <garazbolg@gmail.com>
2025-10-06 00:49:49 +0200

    Step sound

---
+++ Assets/Scripts/AnimAudioEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AnimAudioEvent : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+    public void PlayClip()
+    {
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(clip);
+    }
+}
